04a5140543ceafab086c227aab50f632924bf663
Shubham Saurabh <shubhamsaurabh@outlook.com>
2022-08-03 00:34:17 +0530

    updated responsive code

--- src/components/Shorten.jsx
+++ src/components/Shorten.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 const Shorten = () => {
 	const [input, setInput] = React.useState("");
 	const [data, setData] = React.useState([]);
-	const [clicked , setClicked] = React.useState(false);
+	const [clicked, setClicked] = React.useState(false);
 	const [isAvailable, setIsAvailable] = React.useState(false);
 
 	const fetchUrl = async (input) => {
@@ -33,19 +33,10 @@ const Shorten = () => {
 
 	return (
 		<div className="flex flex-col justify-center items-center w-full h-auto bg-neutralGray mt-48">
-			<div
-				className="w-[80%] rounded-lg -translate-y-[50%]"
-				style={{
-					backgroundImage: `url(${shortenBG})`,
-					backgroundSize: "contain",
-					backgroundRepeat: "no-repeat",
-					backgroundColor: "hsl(257, 27%, 26%)",
-					backgroundPosition: "60px -40px",
-				}}
-			>
-				<div className="flex flex-col justify-center items-center mt-8 pb-8">
+			<div className="w-[80%] rounded-lg -translate-y-[50%] shortly-bg">
+				<div className="flex flex-col md:flex-row md:gap-8 md:px-16 justify-center items-center mt-8 pb-8">
 					<input
-						className="p-4 rounded-md w-[90%]"
+						className="p-4 rounded-md w-[90%] md:w-[85%]"
 						type="text"
 						value={input}
 						onChange={handleInput}
@@ -53,7 +44,7 @@ const Shorten = () => {
 						placeholder="Shorten a link here..."
 					/>
 					<button
-						className="bg-primaryCyan w-[90%] py-3 rounded-md text-[#fff] text-[1.2rem] font-medium mt-8"
+						className="bg-primaryCyan w-[90%] md:w-[15%] py-3 rounded-md text-[#fff] text-[1.2rem] font-medium mt-8 md:mt-0"
 						onClick={() => fetchUrl(input)}
 					>
 						Shorten it!
@@ -63,12 +54,16 @@ const Shorten = () => {
 			{isAvailable && (
 				<div className="w-[80%] flex flex-col justify-center gap-8 items-center mb-8">
 					{data.map((url, index) => (
-						<div className="bg-[#fff] w-full flex flex-col justify-center items-center py-4 rounded-md" key={index}>
-							<h2 className="text-neutralVeryDarkViolet font-bold text-[1.2rem]">{url.input}</h2>
-							<div className="w-full bg-neutralGrayishViolet h-[1px] my-4"></div>
-							<h2 className="text-primaryCyan font-bold text-[1.2rem]">{url.shorten}</h2>
+						<div className="bg-[#fff] w-full flex flex-col md:flex-row justify-center items-center py-4 rounded-md md:px-16" key={index}>
+							<h2 className="text-neutralVeryDarkViolet font-bold text-[1.2rem] md:w-[60%]">{url.input}</h2>
+							<div className="w-full bg-neutralGrayishViolet h-[1px] my-4 md:hidden"></div>
+							<h2 className="text-primaryCyan font-bold text-[1.2rem] md:w-[30%]">{url.shorten}</h2>
 							<button
-								className={clicked ? "bg-primaryViolet w-[90%] py-3 rounded-md text-[#fff] text-[1.2rem] font-medium mt-8" : "bg-primaryCyan w-[90%] py-3 rounded-md text-[#fff] text-[1.2rem] font-medium mt-8"}
+								className={
+									clicked
+										? "bg-primaryViolet w-[90%] md:w-[10%] py-3 rounded-md text-[#fff] text-[1.2rem] font-medium mt-8 md:mt-0"
+										: "bg-primaryCyan w-[90%] md:w-[10%] py-3 rounded-md text-[#fff] text-[1.2rem] font-medium mt-8 md:mt-0"
+								}
 								onClick={() => copyUrl(url.shorten)}
 							>
 								{clicked ? "Copied!" : "Copy"}
